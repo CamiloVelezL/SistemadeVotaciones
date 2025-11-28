91bd74c37cb24fe0757870e4c70b609bef7f4ee0
Camilo Andres Velez Luna <105832950+CamiloVelezL@users.noreply.github.com>
2025-11-28 12:13:36 -0500

    Correccion Update usuario y contraseña Localhost

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     SECRET_KEY = 'voting-system-secret-key-2024'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Queteimporta26@localhost/voting_system'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://Usuario:Cotraseña@localhost/voting_system'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
